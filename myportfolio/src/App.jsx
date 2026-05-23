129a35c0faa40685dfebc6e5a3f637ab3278ca5a
Luc Casassus Latrille <l.casassus-latrille@etud.univ-pau.fr>
2026-05-23 21:10:42 +0200

    started developpement

---
+++ myportfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/NavBar/Navbar';
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
+
---
+++ myportfolio/src/components/NavBar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Navbar.css"
+import logo from "../../assets/d.png"
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src={logo} alt="logo"/>
+      <ul>
+        <li>Home</li>
+        <li>About me</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
