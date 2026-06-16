8a21c2d63f3cba0db375e9618b9f1d902f4d9b84
Luc Casassus Latrille <l.casassus-latrille@etud.univ-pau.fr>
2026-06-16 13:34:04 +0200

    fixed some things

--- myportfolio/src/components/Author/Author.jsx
+++ myportfolio/src/components/Author/Author.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './Author.css'
 import Skill from '../Skill/Skill'
 
-
 const Author = () => {
+
   return (
+    <div className="container">
     <div className="Info-container">
+      <img src="/src/assets/about.png" alt="" />
         <h2>About myself</h2>
-        <p>My name is Luc Casassus Latrille, I am a 20 year old student at ISANUM, 
+        <p>   My name is Luc Casassus Latrille, I am a 20 year old student at ISANUM, \n
             a school of engineering in the south of France. 
             I am currently in the end of my second year of studies and I am passionate about app development and network configuration basics. 
             I have a strong interest in technology and I enjoy creating content related to these topics.
@@ -17,6 +19,7 @@ const Author = () => {
             <li><Skill name="App Development" description="I have experience in app development."/></li>
             <li><Skill name="Network Configuration" description="I have a good understanding of network protocols and I am familiar with configuring and managing Cisco's network systems."/></li>
         </ul>
+    </div>
     </div>
   )
 }
